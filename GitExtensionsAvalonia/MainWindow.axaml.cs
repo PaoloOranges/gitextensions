e14cf01a815c815b5d66aab990764b4f8b731216
Paolo Oranges <paolo.oranges@gmail.com>
2024-12-30 17:22:21 +0100

    Add Avalonia Start Project

---
+++ GitExtensionsAvalonia/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GitExtensionsAvalonia;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
